535e1f80b61defe14ea9ebf2fd7c6e4a6b8c04aa
axwii <axwi0001@stud.kea.dk>
2024-10-10 12:15:16 +0200

    Refactor animal list display and add styling

---
+++ Animalbase/animals_objects.js
@@ -0,0 +1,100 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+const allAnimals = [
+    {
+        name: "Mandu",
+        desc: "amazing",
+        type: "cat",
+        age: 10
+    },
+    {
+        name: "Mia",
+        desc: "black",
+        type: "cat",
+        age: 10
+    },
+    {
+        name: "Leeloo",
+        desc: "growing",
+        type: "dog",
+        age: 3
+    },
+    {
+        name: "Toothless",
+        desc: "trained",
+        type: "dragon",
+        age: 14
+    },
+    {
+        name: "ScoobyDoo",
+        desc: "wondering",
+        type: "dog",
+        age: 58
+    },
+    {
+        name: "Horsey",
+        desc: "horsing",
+        type: "horse",
+        age: 10
+    }
+]
+
+function start( ) {
+    console.log("ready");
+
+    loadJSON();
+}
+
+
+function loadJSON() {
+    fetch("animals.json")
+    .then( response => response.json() )
+    .then( jsonData => {
+        // when loaded, prepare objects
+        prepareObjects( jsonData );
+    });
+}
+
+function prepareObjects( jsonData ) {
+    jsonData.forEach( jsonObject => {
+        // TODO: Create new object with cleaned data - and store that in the allAnimals array
+        const fullname = jsonObject.fullname;
+        const [name, , desc, type] = fullname.split(" ");
+
+        let animal = {
+            name: name,
+            desc: desc,
+            type: type,
+            age: jsonObject.age
+        };
+    
+        allAnimals.push( animal );
+        // TODO: MISSING CODE HERE !!!
+    });
+
+    displayList();
+}
+
+function displayList() {
+    // clear the list
+    document.querySelector("#list tbody").innerHTML = "";
+
+    // build a new list
+    allAnimals.forEach( displayAnimal );
+}
+
+function displayAnimal( animal ) {
+    // create clone
+    const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+    // set clone data
+    clone.querySelector("[data-field=name]").textContent = animal.name;
+    clone.querySelector("[data-field=desc]").textContent = animal.desc;
+    clone.querySelector("[data-field=type]").textContent = animal.type;
+    clone.querySelector("[data-field=age]").textContent = animal.age;
+
+    // append clone to list
+    document.querySelector("#list tbody").appendChild( clone );
+}
